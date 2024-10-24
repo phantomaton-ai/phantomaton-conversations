35052fa606cb040702d5639595061176398981d4
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-23 19:32:50 -0700

    okay

--- phantomaton-conversations.test.js
+++ phantomaton-conversations.test.js
@@ -1,10 +1,11 @@
+import hierophant from 'hierophant';
 import { expect } from 'lovecraft';
 
+import Conversation from './conversation.js';
 import conversations from './phantomaton-conversations.js';
 
 describe('phantomaton-conversations', () => {
   let plugin;
-  let container;
 
   beforeEach(() => {
     plugin = conversations();
@@ -14,5 +15,12 @@ describe('phantomaton-conversations', () => {
     expect(conversations.user).to.exist;
     expect(conversations.assistant).to.exist;
     expect(conversations.conversation).to.exist;
+  });
+
+  it('provides resolvers/providers to install', () => {
+    const container = hierophant();
+    plugin.install.forEach(component => container.install(component));
+    const conversation = container.resolve(conversations.conversation.resolve);
+    expect(conversation[0]()).instanceof(Conversation);
   });
 });
